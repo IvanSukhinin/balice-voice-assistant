04e4690f4a2ee1e8bbe401127faea248c8a1d563
hellokitty <ssssshazam@gmail.com>
2023-04-15 23:55:10 +0700

    Implement a separate call to chatgpt

--- src/bot/bot_command_mapping.py
+++ src/bot/bot_command_mapping.py
@@ -4,9 +4,11 @@ from bot.bot_skills import BotSkills
 class BotCommandMapping():
 	def __init__(self, bot_config):
 		self.__bot_skills = BotSkills(bot_config['default_notification'], bot_config['answers'])
+		self.__is_gpt_calling = False
 		self.__is_calling = False
 		self.__is_in_process = False
 		self.__bot_names = bot_config['names']
+		self.__chat_gpt_names = bot_config['chat_gpt_names']
 		self.__bot_commands = bot_config['commands']
 		self.__command_accurancy = bot_config['command_map_accuracy']
 
@@ -20,11 +22,19 @@ class BotCommandMapping():
 
 		print(cmd)
 
-		# start recognize
 		if self.__check_command(cmd, self.__bot_names) and not self.__is_calling:
 			self.__is_calling = True
 			self.__bot_skills.playWelcomeSound()
 			return True
+		elif self.__check_command(cmd[:len(max(self.__chat_gpt_names, key=len))], self.__chat_gpt_names) and not self.__is_gpt_calling:
+			self.__is_gpt_calling = True
+			self.__bot_skills.playWelcomeSound()
+			return True
+
+		if self.__is_gpt_calling:
+			self.__is_gpt_calling = False
+			self.__bot_skills.terminate_process()
+			return self.__bot_skills.say_chat_gpt(cmd)
 
 		if not self.__is_calling:
 			return False
@@ -33,15 +43,12 @@ class BotCommandMapping():
 		self.__is_in_process = self.__bot_skills.is_in_process()
 
 		# kill process if need
-		if self.__check_command(cmd, self.__bot_commands['terminate']) and self.__is_in_process:
+		if self.__check_command(cmd, self.__bot_commands['terminate']):
 			return self.__bot_skills.terminate_process()
 
 		# if bot has an active process, just abort
 		if self.__is_in_process:
 			return False
-
-		if self.__check_command(cmd[:len(self.__bot_commands['chat_gpt'])], self.__bot_commands['chat_gpt']):
-			return self.__bot_skills.say_chat_gpt(cmd)
 
 		if self.__check_command(cmd, self.__bot_commands['weather']):
 			return self.__bot_skills.say_weather()
